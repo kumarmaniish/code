462fa62eaa19c52031a2cf64a4a6a9e9d6b700dc
Manish Kumar <133526263+kumarmaniish@users.noreply.github.com>
2024-09-11 16:34:16 +0530

    Update ArrayString.cpp

--- code_cpp/ImportantQuestions/ArrayString.cpp
+++ code_cpp/ImportantQuestions/ArrayString.cpp
@@ -65,3 +65,69 @@ int main() {
 
     return 0;
 }
+
+
+//maximum
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMax(vector<int> arr){
+    
+    int max=arr[0];
+    for(int i=1; i<arr.size(); i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int main() {
+    // Write C++ code here
+    cout<<"Enter the number of Semster : ";
+    int n; cin>>n; 
+    cout<<endl;
+    
+    cout<<"Enter the number of subjects in semster 1 : ";
+    int a; cin>>a;
+    cout<<endl;
+    
+    cout<<"Enter the number of subjects in semster 2 : ";
+    int b; cin>>b;
+    cout<<endl;
+    
+    cout<<"Enter the number of subjects in semster 3 : ";
+    int c; cin>>c;
+    cout<<endl;
+    
+    cout<<"Marks obtained in semster 1 is : ";
+    vector<int> sem1(a);
+    for(int i=0; i<a; i++){
+        cin>>sem1[i];
+    }
+    cout<<endl;
+    
+    cout<<"Marks obtained in semster 23 is : ";
+    vector<int> sem2(b);
+    for(int i=0; i<b; i++){
+        cin>>sem2[i];
+    }
+    cout<<endl;
+    
+    cout<<"Marks obtained in semster 3 is : ";
+    vector<int> sem3(c);
+    for(int i=0; i<c; i++){
+        cin>>sem3[i];
+    }
+    cout<<endl;
+    
+    cout<<"Maximum marks in 1st Semster is : ";
+    cout<<findMax(sem1)<<endl;;
+    cout<<"Maximum marks in 1st Semster is : ";
+    cout<<findMax(sem2)<<endl;
+    cout<<"Maximum marks in 1st Semster is : ";
+    cout<<findMax(sem3)<<endl;
+
+    return 0;
+}
